0c80772bea33a52e8b9be38d525ef3710f237cba
Wshape1 <Wshape1@users.noreply.github.com>
2017-03-05 09:53:23 +0800

    Update WMain.php

--- src/WsRandomChatColor/WMain.php
+++ src/WsRandomChatColor/WMain.php
@@ -16,7 +16,7 @@ public function onEnable(){
 	}
 	public function Chat(PlayerChatEvent $event){
 	$msg=$event->getMessage();
-	$cl=["§0","§1","§2","§3","§4","§5","§6","§7","§8","§9","§a","§b","§c","§d","§e","§f",];
+	$cl=["§0","§1","§2","§3","§4","§5","§6","§7","§8","§9","§a","§b","§c","§d","§e","§f"];
 	$c=$cl[mt_rand(0,15)];
 	$event->setMessage($c.$msg);
 	}
